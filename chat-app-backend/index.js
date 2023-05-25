4be1e0fa5db1b052ab2c8ec136685e443949c54e
Yang <40138451+HiMenma@users.noreply.github.com>
2023-05-25 17:43:19 +0800

    update

--- chat-app-backend/index.js
+++ chat-app-backend/index.js
@@ -1,39 +1,43 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-
 const app = express();
-const port = process.env.PORT || 3000;
-
+const port = 3000;
+const apiKey = 'your api token'; // 替换为你的 OpenAI API 密钥
 app.use(cors());
 app.use(express.json());
 
 app.post('/chat', async (req, res) => {
   const userInput = req.body.userInput;
-  console.log("userInput: " + userInput);
+  res.setHeader('Transfer-Encoding', 'chunked');
+  const requestBody = {
+    model: 'gpt-3.5-turbo', // 模型版本
+    messages: [
+      { role: 'system', content: 'You are a helpful assistant.' },
+      { role: 'user', content: userInput }
+    ]
+  };
+
   try {
-    // 调用 ChatGPT API
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', {
-    //   prompt: userInput,
-      model: "gpt-3.5-turbo",
-      messages: [{"role": "user", "content": userInput}],
-    //   max_tokens: 50,
-    //   n: 1,
-    //   stop: null,
-    //   temperature: 0.5,
-    }, {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, {
       headers: {
-        // 'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
-        'Authorization': `Bearer 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`,
         'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
       },
+      responseType: 'stream' // 设置响应类型为流
     });
 
-    const reply = response.data.choices[0].text.trim();
-    res.json({ reply });
+    if (response.status === 200) {
+      res.type('text/plain'); // 设置响应类型为纯文本
+
+      response.data.pipe(res); // 将响应流直接发送给客户端
+    } else {
+      console.error('Error:', response.status);
+      res.status(500).json({ error: 'An error occurred' });
+    }
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'An error occurred while processing your request.' });
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
   }
 });
 
--- chat-app/src/components/ChatBox.vue
+++ chat-app/src/components/ChatBox.vue
@@ -1,62 +1,80 @@
 <template>
-    <div>
+  <div>
+    <div style="height: 400px" class="aaa">
       <div v-for="message in messages" :key="message.id">
-        <div v-if="message.type === 'user'">
-          <span class="user-message">{{ message.text }}</span>
-        </div>
-        <div v-if="message.type === 'bot'">
-          <span class="bot-message">{{ message.text }}</span>
-        </div>
+      <div v-if="message.type === 'user'">
+        <span class="user-message">{{ message.text }}</span>
       </div>
-      <div class="input-container">
-        <input class="input-field" v-model="inputText" @keydown.enter="sendMessage" placeholder="Type a message" />
-        <!-- <button class="send-button" @click="sendMessage">Send</button> -->
-        <button class="send-button" @click="sendMessage">
-            <font-awesome-icon icon="comment" /> Send
-        </button>
+      <div v-if="message.type === 'bot'">
+        <span class="bot-message">{{ message.text }}</span>
       </div>
     </div>
-  </template>
+    </div>
+    
+    <div style="width: 100%;" class="input-container">
+      <input style="width: 400px;" class="input-field" v-model="inputText" @keydown.enter="sendMessage" placeholder="Type a message" />
+      <!-- <button class="send-button" @click="sendMessage">Send</button> -->
+      <button class="send-button" @click="sendMessage">
+        <font-awesome-icon icon="comment" /> Send
+      </button>
+    </div>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        messages: [],
-        inputText: ''
-      };
-    },
-    methods: {
-      async sendMessage() {
-        const userInput = this.inputText;
-  
-        // 发送用户输入给后端接口
-        const response = await fetch('http://localhost:3000/chat', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ userInput })
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          const botReply = data.reply;
-  
-          // 添加用户输入和机器人回复到消息列表
-          this.messages.push({ id: Date.now(), text: userInput, type: 'user' });
-          this.messages.push({ id: Date.now() + 1, text: botReply, type: 'bot' });
-        } else {
-          console.error('Error:', response.status);
-        }
-  
-        this.inputText = '';
-      }
+<script>
+export default {
+  data() {
+    return {
+      messages: [],
+      inputText: ''
+    };
+  },
+  methods: {
+    async sendMessage() {
+      const userInput = this.inputText;
+
+      // 打印用户输入
+      this.messages.push({
+        id:Date.now(),
+        text: userInput,
+        type: 'user'
+      });
+
+
+      const response = await fetch('http://localhost:3000/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userInput })
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        let botReply = data.choices[0].message.content;
+
+    // Split the response into individual lines and add a newline character every 50 characters using a for loop
+    let lines = [];
+    for (let i = 0; i < botReply.length; i += 50) {
+      lines.push(botReply.substr(i, 50));
     }
-  };
-  </script>
+    botReply = lines.join('\n');
+
+        // Split the response into individual lines and add each line as a separate message
+        botReply.split('\n').forEach(line => {
+          this.messages.push({ id: Date.now(), text: line, type: 'bot' });
+        });
+      } else {
+        console.error('Error:', response.status);
+      }
+
+      this.inputText = '';
+    }
+  }
+};
+</script>
   
-  <style>
+<style>
 .container {
   min-height: 100vh;
   display: flex;
@@ -70,7 +88,7 @@
 }
 
 .input-container {
-  display: flex;
+  /* display: flex; */
   align-items: center;
   margin-top: 10px;
 }
@@ -79,6 +97,7 @@
   flex: 1;
   padding: 8px;
   border-radius: 5px;
+  width: 500px;
   border: 1px solid #ccc;
   margin-right: 10px;
 }
@@ -90,5 +109,14 @@
   color: white;
   border: none;
   cursor: pointer;
+}
+.aaa {
+  width: 600px;
+  margin: auto;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+.aaa:hover {
+  background: #f5f3f3;
 }
 </style>
